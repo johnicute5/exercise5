074a774b285c9b1593b4d72402709d00580ac074
johnicute5 <johnicoirinco.hs@gmail.com>
2022-09-26 22:42:48 +0800

    withmodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { CommandBarComponent } from './shared/components/command-bar/command-bar.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlogFormComponent } from './modules/blog/pages/blog-form/blog-form.component';
+import { BookFormComponent } from './modules/book/pages/book-form/book-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CommandBarComponent
+    CommandBarComponent,
+    BlogFormComponent,
+    BookFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/components/command-bar/command-bar.component.ts
+++ src/app/shared/components/command-bar/command-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Blog } from 'src/app/blog/models/blog';
 
 @Component({
   selector: 'app-command-bar',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./command-bar.component.scss']
 })
 export class CommandBarComponent implements OnInit {
+  @Input() blogs:Blog | undefined;
+  @Output() addEmitter = new EventEmitter();
+  @Output() deleteAllEmitter = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  add(){
+    this.addEmitter.emit(this.blogs)
 
+  }
+  deleteAll(){
+    this.deleteAllEmitter.emit(this.blogs)
+  }
 }
