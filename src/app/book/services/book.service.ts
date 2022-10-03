58230cbb845b8cd02b06b5686d9856d75222778e
johnicute5 <johnicoirinco.hs@gmail.com>
2022-10-04 01:37:19 +0800

    withDeleteUpdateSoon

--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, filter, Subject, ReplaySubject, BehaviorSubject, throwError } from 'rxjs'
+import { Observable, map, filter, Subject, ReplaySubject, BehaviorSubject, throwError, catchError } from 'rxjs'
 import { environment } from 'src/environments/environment';
 import { Book } from '../models/book';
 
@@ -12,6 +12,7 @@ export class BookService {
   subject = new Subject();
   replay = new ReplaySubject(10);
   behavior = new BehaviorSubject("Jerome");
+  apiURL = 'http://localhost:3000';
   constructor(private http:HttpClient) {}
   private handleError(error: any) {
     console.log(error);
@@ -37,9 +38,27 @@ export class BookService {
     return this.http.put(`${environment.url}/books/${books.id}`, books).subscribe()
   }
   deleteBooks(id:number) {
-    return this.http.delete(`${environment.url}/Books/${id}`).subscribe()
+    return this.http.delete(`${environment.url}/books/${id}`).subscribe()
   }
 
+
+  find(id: number): Observable<Book> {
+    return this.http.get<Book>(this.apiURL + `/books/${id}`)
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  errorHandler(error: { error: { message: string; }; status: any; message: any; }) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+ }
+}
 
   // showBooks() {
   //   return this.arrayOfBooks;
@@ -48,4 +67,3 @@ export class BookService {
   //   return this.arrayOfBooks.find(book =>book.id === id);
 
   // }
-}
